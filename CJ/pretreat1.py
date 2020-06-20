d7e9a6659884c6b360ea9800210c5e0bdd588413
Simba <leaping96@ajou.ac.kr>
2020-06-20 22:44:35 +0900

    CJ 중량추출 기능수정

--- CJ/pretreat1.py
+++ CJ/pretreat1.py
@@ -1,13 +1,16 @@
 #외부모듈
 import openpyxl
-import os
 from openpyxl import load_workbook
 from openpyxl import Workbook
+import os
 
 #내부모듈
 from module import init
+from module import pname
 from module import prod
-
+from module import stat
+from module import tax
+from module import wt
 
 output_excel = Workbook()
 output_sheet = output_excel.active
@@ -26,10 +29,21 @@ cnt=2
 while input_sheet.cell(cnt, 1).value:
     print(cnt)#행 번호출력
     raw = init.string(input_sheet,output_sheet,cnt)#원본행 로드
-    producer = prod.pre(raw,maker_sheet, nation_sheet)
-    output_sheet.cell(cnt, 13, producer)  #생산자명
+
+    #productName = pname.pre(raw)
+    #producer = prod.pre(raw,maker_sheet, nation_sheet)
+    #status = stat.pre(raw)
+    #taxataion = tax.pre(raw)
+    weight = wt.pre(raw)
+
+    #output_sheet.cell(cnt, 10, productName)  #상품명
+    output_sheet.cell(cnt, 12, weight)  # 중량
+    #output_sheet.cell(cnt, 13, producer)  #생산자명
+    #output_sheet.cell(cnt, 14, taxataion)  # 면과세여부
+    #output_sheet.cell(cnt, 15, status)  #보관상태
+
     cnt=cnt+1
 
 os.chdir('CJ')
-output_excel.save(os.getcwd()+"/output/CJ_prod.xlsx")
+output_excel.save(os.getcwd()+"/output/CJ_1.xlsx")
 
